c98e7cfa53e3252fec50d703e4496b0f18b39f86
Lucas Velazquez <Lucas-Velazquez@aj.com.py>
2023-02-19 09:29:42 -0300

    forward and backward functions now return Result<String, Vec<u8>> of the shifted text. Tests mod moved into lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,74 @@
 
 use encoding::{Encoding, DecoderTrap, EncoderTrap, all::WINDOWS_1253 as CODEC};
 
-/// magic words to trigger the dark ritual: "SIAC-UNJBG"
-const MAGIC:  [u8; 10] = [83, 73, 65, 67, 45, 85, 78, 74, 66, 71]; // "SIAC-UNJBG"
-/// 9usize
-const MAGICL: usize    = MAGIC.len() - 1; // I don't know
-
-#[cfg(test)] mod test;
+/// magic words to trigger the (default) dark ritual: "SIAC-UNJBG"
+// pub const MAGIC:  [u8; 10] = [83, 73, 65, 67, 45, 85, 78, 74, 66, 71]; // "SIAC-UNJBG"
+pub const MAGIC:  [u8; 9] = [83, 73, 65, 67, 45, 85, 78, 74, 66]; // "SIAC-UNJB"
 
 // pub fn decypher(_original: &str, _encoded: &str) -> Vec<u8> { Vec::new() }
 
-pub fn forward(text: &str) -> String {
+pub fn forward(text: &str, cypher: &[u8]) -> Result<String, Vec<u8>> {
     let mut o: Vec<u8> = CODEC.encode(text, EncoderTrap::Strict).unwrap();
 
     for (n, c) in o.iter_mut().enumerate() {
-        *c = c.wrapping_add(MAGIC[n % MAGICL]);
+        *c = c.wrapping_add(cypher[n % cypher.len()]);
     };
 
-    return CODEC.decode(o.as_slice(), encoding::DecoderTrap::Strict).unwrap();
+    return match CODEC.decode(o.as_slice(), DecoderTrap::Strict) {
+        Ok(so) => Ok(so),
+        Err(_) => Err(o),
+    };
 }
 
-pub fn backward(text: &str) -> String {
+pub fn backward(text: &str, cypher: &[u8]) -> Result<String, Vec<u8>> {
     let mut o: Vec<u8> = CODEC.encode(text, EncoderTrap::Strict).unwrap();
 
     for (n, c) in o.iter_mut().enumerate() {
-        *c = c.wrapping_sub(MAGIC[n % MAGICL]);
+        *c = c.wrapping_sub(cypher[n % cypher.len()]);
     };
 
-    return CODEC.decode(o.as_slice(), encoding::DecoderTrap::Strict).unwrap();
-
+    return match CODEC.decode(o.as_slice(), DecoderTrap::Strict) {
+        Ok(so) => Ok(so),
+        Err(_) => Err(o),
+    };
 }
+
+#[cfg(test)]
+mod test {
+    static TESTS: [(&'static str, &'static str); 1] = [
+        ("10.10.70.32", "„yot]ƒ…zp†{" )
+    ]; 
+
+    #[test] fn forwards() {
+    for (s, d) in TESTS {
+        let r: String = super::forward(s, &super::MAGIC).unwrap();
+        std::fs::write("foo.txt", r.clone()).unwrap();
+        println!("{s} => {}", r.as_str());
+        assert_eq!(r.as_str(), d);
+    };
+    }
+
+    #[test] fn backward() {
+    for (s, d) in TESTS {
+        let r = super::backward(d, &super::MAGIC).unwrap();
+        std::fs::write("foo.txt", r.clone()).unwrap();
+        println!("{d} => {}", r.as_str());
+        assert_eq!(s, r.as_str());
+    };
+    }
+
+    #[test] fn roundtrip() {
+        for (s, d) in TESTS {
+            let r: String = super::forward(s, &super::MAGIC).unwrap();
+            let b: String = super::backward(r.as_str(), &super::MAGIC).unwrap();
+            println!("{s} => {} => {}", r.as_str(), b.as_str());
+            assert_eq!(b.as_str(), s);
+
+            let r: String = super::backward(d, &super::MAGIC).unwrap();
+            let b: String = super::forward(r.as_str(), &super::MAGIC).unwrap();
+            println!("{d} => {} => {}", r.as_str(), b.as_str(),);
+            assert_eq!(super::forward(r.as_str(), &super::MAGIC).unwrap().as_str(), d);
+        };
+    }
+}
+
